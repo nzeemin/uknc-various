3a4c9d9cb7c8f42b22759d9cc4dda62846a216e5
Nikita Zimin <nzeemin@gmail.com>
2021-04-12 21:07:21 +0300

    WIP

--- STALK-PASCAL/!compilelink.bat
+++ STALK-PASCAL/!compilelink.bat
@@ -9,8 +9,9 @@ for /F "delims=#" %%E in ('"prompt #$E# & for %%E in (1) do rem"') do set "ESCch
 @if exist FRANDU.OBJ del FRANDU.OBJ
 @if exist STALK1.MAC del STALK1.MAC
 @if exist STALK1.LST del STALK1.LST
-@if exist STALK1.OBJ del STALK1.OBJ
-@if exist STALK1.SAV del STALK1.SAV
+@if exist STALKP.LST del STALKP.LST
+@if exist STALKP.OBJ del STALKP.OBJ
+@if exist STALKP.SAV del STALKP.SAV
 
 %rt11exe% MACRO/LIST:DK: FRANDU.MAC
 
@@ -35,25 +36,43 @@ REM  findstr /RC:"^****** " STALK1.LST
   exit /b
 )
 
-@if exist STALK1.LST del STALK1.LST
+@if exist STALKP.MAC del STALKP.MAC
 
-%rt11exe% MACRO/LIST:DK: STALK1.MAC
+REM Patching the STALK1.MAC file replacing KOI-7 chars with KOI-8 ones
+set replaces= -replace ^
+ ':\t.WORD\t25120\r\n\t.WORD\t23109\r\n\t.WORD\t19488\r\n\t.WORD\t21317\r\n\t.WORD\t20052\r\n\t.WORD\t17225\r\n\t.WORD\t16217', ^
+ ':	.BYTE 32,226,197,218,32,204,197,211,212,206,201,195,217,63' -replace ^
+ ':\t.WORD\t2378\r\n\t.WORD\t20336\r\n\t.WORD\t23108\r\n\t.WORD\t19781\r\n\t.WORD\t19525\r\n\t.WORD\t17752\r\n\t.WORD\t32', ^
+ ':	.BYTE 74,9,240,207,196,218,197,205,197,204,216,197,32,0' -replace ^
+ ':\t.WORD\t28192\r\n\t.WORD\t17696\r\n\t.WORD\t19488\r\n\t.WORD\t22560\r\n\t.WORD\t23072\r\n\t.WORD\t20768', ^
+ ':	.BYTE 32,238,32,197,32,204,32,216,32,218,32,209' -replace ^
+ ':\t.WORD\t8314\r\n\t.WORD\t8279\r\n\t.WORD\t8261\r\n\t.WORD\t8274\r\n\t.WORD\t8280\r\n\t.WORD\t33', ^
+ ':	.BYTE 250,32,215,32,197,32,210,32,216,32,33,0' -replace ^
+ ':\t.WORD\t16750\r\n\t.WORD\t23584\r\n\t.WORD\t20308\r\n\t.WORD\t8266\r\n\t.WORD\t21595\r\n\t.WORD\t19285\r\n\t.WORD\t8261\r\n\t.WORD\t16730\r\n\t.WORD\t19531\r\n\t.WORD\t21585\r\n\t.WORD\t17737\r\n\t.WORD\t0', ^
+ ':	.BYTE 238,193,32,220,212,207,202,32,219,212,213,203,197,32,218,193,203,204,209,212,201,197,0,0'
+powershell -Command "(Get-Content STALK1.MAC -Raw) %replaces% | Out-File -encoding ASCII STALK1P.MAC"
+
+REM Adding BLK000.INC header to the patched file
+copy BLK000.INC+STALK1P.MAC STALKP.MAC
+del STALK1P.MAC
+
+%rt11exe% MACRO/LIST:DK: STALKP.MAC
 
 set errdet=
-for /f "delims=" %%a in ('findstr /B "Errors detected" STALK1.LST') do set "errdet=%%a"
+for /f "delims=" %%a in ('findstr /B "Errors detected" STALKP.LST') do set "errdet=%%a"
 if "%errdet%"=="Errors detected:  0" (
-  echo STALK1.MAC COMPILED SUCCESSFULLY
+  echo STALKP.MAC COMPILED SUCCESSFULLY
 ) ELSE (
-  findstr /RC:"^[ABDEILMNOPQRTUZ] " STALK1.LST
+  findstr /RC:"^[ABDEILMNOPQRTUZ] " STALKP.LST
   echo ======= %errdet% =======
   exit /b
 )
 
-%rt11exe% LINK/STACK:1000 STALK1,FRANDU,PASCAL /MAP:STALK1.MAP
+%rt11exe% LINK/STACK:1000 STALKP,FRANDU,PASCAL /MAP:STALKP.MAP
 
-for /f "delims=" %%a in ('findstr /B "Undefined globals" STALK1.MAP') do set "undefg=%%a"
+for /f "delims=" %%a in ('findstr /B "Undefined globals" STALKP.MAP') do set "undefg=%%a"
 if "%undefg%"=="" (
-REM  type STALK1.MAP
+REM  type STALKP.MAP
   echo.
   echo %ESCchar%[92mLINKED SUCCESSFULLY%ESCchar%[0m
 ) ELSE (
