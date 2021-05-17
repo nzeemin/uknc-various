d7bb0c27716d0608ec32425ca68fc5ce2bd62c1f
Nikita Zimin <nzeemin@gmail.com>
2021-05-18 00:41:14 +0300

    KINGOM-PASCAL WIP

--- KINGOM-PASCAL/!compilelink.bat
+++ KINGOM-PASCAL/!compilelink.bat
@@ -10,16 +10,19 @@ if exist KINGM2.MAC del KINGM2.MAC
 if exist KINGM3.MAC del KINGM3.MAC
 if exist KINGM4.MAC del KINGM4.MAC
 if exist KINGM5.MAC del KINGM5.MAC
+if exist KINGM6.MAC del KINGM6.MAC
 if exist KINGOM.LST del KINGOM.LST
 if exist KINGM2.LST del KINGM2.LST
 if exist KINGM3.LST del KINGM3.LST
 if exist KINGM4.LST del KINGM4.LST
 if exist KINGM5.LST del KINGM5.LST
+if exist KINGM6.LST del KINGM6.LST
 if exist KINGOM.OBJ del KINGOM.OBJ
 if exist KINGM2.OBJ del KINGM2.OBJ
 if exist KINGM3.OBJ del KINGM3.OBJ
 if exist KINGM4.OBJ del KINGM4.OBJ
 if exist KINGM5.OBJ del KINGM5.OBJ
+if exist KINGM6.OBJ del KINGM6.OBJ
 if exist KINGOM.SAV del KINGOM.SAV
 
 %rt11exe% @CPAS
@@ -74,6 +77,16 @@ REM  findstr /RC:"^****** " KINGM5.LST
   exit /b
 )
 
+set errdet=
+for /f "delims=" %%a in ('findstr /B "ERRORS DETECTED" KINGM6.LST') do set "errdet=%%a"
+if "%errdet%"=="ERRORS DETECTED:  0    " (
+  rem echo KINGM6.PAS COMPILED SUCCESSFULLY
+) ELSE (
+REM  findstr /RC:"^****** " KINGM6.LST
+  echo ======= KINGM6.PAS NOT COMPILED =======
+  exit /b
+)
+
 %rt11exe% @CMACRO
 
 set errdet=
@@ -122,6 +135,16 @@ if "%errdet%"=="Errors detected:  0" (
   rem echo KINGM5.MAC COMPILED SUCCESSFULLY
 ) ELSE (
   findstr /RC:"^[ABDEILMNOPQRTUZ] " KINGM5.LST
+  echo ======= %errdet% =======
+  exit /b
+)
+
+set errdet=
+for /f "delims=" %%a in ('findstr /B "Errors detected" KINGM6.LST') do set "errdet=%%a"
+if "%errdet%"=="Errors detected:  0" (
+  rem echo KINGM6.MAC COMPILED SUCCESSFULLY
+) ELSE (
+  findstr /RC:"^[ABDEILMNOPQRTUZ] " KINGM6.LST
   echo ======= %errdet% =======
   exit /b
 )
